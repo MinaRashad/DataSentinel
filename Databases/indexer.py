131259d33af54d59666a8935f3fe5cf0a9bbd09a
Mina Eskandar <Dev.minaeskandar@gmail.com>
2023-07-02 17:57:26 -0400

    indexer and watchlist added

--- Databases/indexer.py
+++ Databases/indexer.py
@@ -1,36 +1,88 @@
 import os
 import sys
 
-def split_file(dataset_path, chunk_size):
-    folder_name = os.path.splitext(dataset_path)[0]
+def split_file(dataset_path, chunk_size, watchlist=[]):
+    # take everything after / 
+    breaches = []
+    folder_name = dataset_path.split('/')[-1] + '_chunked'
+
     os.makedirs(folder_name, exist_ok=True)
 
-    with open(dataset_path, 'r') as dataset_file:
-        chunk_num = 1
-        lines = dataset_file.readlines()
-        num_lines = len(lines)
-        start_idx = 0
-        end_idx = chunk_size
+    # read file stream
+    chunk_num = 1
+    with open(dataset_path, 'rb') as dataset_file:
+        while True:
+            chunk = dataset_file.read(chunk_size)
+            if not chunk:
+                break
 
-        while start_idx < num_lines:
-            chunk_lines = lines[start_idx:end_idx]
+            # check if last character is \n
+            while chunk[-1] != 10:
+                # read more bytes until \n is found
+                char = dataset_file.read(1)
+                if not char:
+                    break
+                chunk += char
+                # break if EOF
+                
 
-            chunk_filename = os.path.join(folder_name, f'chunk_{chunk_num}.txt')
-            with open(chunk_filename, 'w') as chunk_file:
-                chunk_file.writelines(chunk_lines)
+            # check if chunk contains any words from watchlist
+            if watchlist:
+                decoded_chunk = chunk.decode('latin-1')
 
+                for line in watchlist:
+                    # index of line in chunk
+                    idx = decoded_chunk.find(line)
+
+                    if idx != -1:
+                        print(f'Found {line} in chunk {chunk_num} at index {idx}')
+                        # get \n index before and after idx
+                        
+                        line_start = decoded_chunk.rfind('\n', 0, idx)
+                        line_end = decoded_chunk.find('\n', idx)
+
+                        # get line from chunk
+                        breach = decoded_chunk[line_start:line_end]
+                        breaches.append(breach)
+                        print(f'{breach}')
+                        
+            # write chunk to file
+            with open(f'{folder_name}/chunk_{chunk_num}.txt', 'wb') as chunk_file:
+                chunk_file.write(chunk)
             chunk_num += 1
-            start_idx = end_idx
-            end_idx += chunk_size
 
-    print(f'Splitting complete. {chunk_num - 1} files created in the folder: {folder_name}')
+
+    print(f'{chunk_num-1} files created in {folder_name}')
+    
+    if breaches:
+        print(f"Alert! Found {len(breaches)} breaches in {dataset_path}")
+        for breach in breaches:
+            print(f'{breach}')
+    else:
+        print(f'No breaches found in {dataset_path}')
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python indexer.py <dataset_path>')
+    if len(sys.argv) < 2:
+        print('Usage: python indexer.py <dataset_path> [--watchlist <watchlist_path>]')
         sys.exit(1)
 
     dataset_path = sys.argv[1]
-    chunk_size = 10000
 
-    split_file(dataset_path, chunk_size)
+    # get --watchlist argument
+    watchlist_path = None
+    if len(sys.argv) == 4:
+        if sys.argv[2] == '--watchlist':
+            watchlist_path = sys.argv[3]
+
+    chunk_size = 10 * 1024 * 1024 # 10 MB
+
+    watchlist = []
+
+    if watchlist_path:
+        with open(watchlist_path, 'r') as watchlist_file:
+            watchlist = watchlist_file.readlines()
+        
+        watchlist = [word.strip() for word in watchlist]
+        
+
+    split_file(dataset_path, chunk_size, watchlist)
